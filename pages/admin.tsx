e86deb28eee108f317402eb7f4228ab28effffda
Kamil Lewiński <lewinski.coding@gmail.com>
2024-10-05 12:35:57 +0200

    fixed reload bug

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -172,7 +172,6 @@ export const getStaticProps: GetStaticProps = async () => {
 
   return {
     props: data,
-    revalidate: 60,
   };
 };
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -173,7 +173,6 @@ export const getStaticProps: GetStaticProps = async () => {
 
   return {
     props: data,
-    revalidate: 60,
   };
 };
 
